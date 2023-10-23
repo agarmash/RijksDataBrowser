fb0c6ad4f439e13557162727569c5816367876db
Artem Garmash <a.garmash.dev@gmail.com>
2023-10-23 17:26:59 +0200

    Initial Commit

---
+++ RijksDataBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RijksDataBrowser
+//
+//  Created by Artem Garmash on 23/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RijksDataBrowserUITests/RijksDataBrowserUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RijksDataBrowserUITestsLaunchTests.swift
+//  RijksDataBrowserUITests
+//
+//  Created by Artem Garmash on 23/10/2023.
+//
+
+import XCTest
+
+final class RijksDataBrowserUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
