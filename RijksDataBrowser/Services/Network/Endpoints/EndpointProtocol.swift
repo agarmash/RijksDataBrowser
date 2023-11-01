57138e3a9d8daa72c42bdbcac37f51de53697162
Artem Garmash <a.garmash.dev@gmail.com>
2023-11-01 18:52:35 +0100

    Move request parsing code into NetworkClient

--- RijksDataBrowser/Services/Network/Endpoints/EndpointProtocol.swift
+++ RijksDataBrowser/Services/Network/Endpoints/EndpointProtocol.swift
@@ -11,24 +11,4 @@ protocol EndpointProtocol {
     associatedtype Response: Decodable
 
     func makeRequest() throws -> URLRequest
-    func parseResponse(from data: Data) throws -> Response
-}
-
-enum EndpointError: Error {
-    case invalidData(Error)
-}
-
-extension EndpointProtocol {
-    func parseResponse(from data: Data) throws -> Response {
-        let decoder = JSONDecoder()
-        
-        let dto: Response
-        do {
-            dto = try decoder.decode(Response.self, from: data)
-        } catch {
-            throw EndpointError.invalidData(error)
-        }
-        
-        return dto
-    }
 }
